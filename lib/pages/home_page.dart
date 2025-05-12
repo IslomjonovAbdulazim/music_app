d8a9a01a69ad021937132123a8c87a867fb9f859
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-12 17:39:08 +0500

    imp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,7 +5,68 @@ List<MediaItem> mediaItems = [
   MediaItem(
     id: "assets://assets/Rihanna - California King Bed.mp3",
     title: "California King Bed",
-
+    artUri: Uri.parse("https://upload.wikimedia.org/wikipedia/commons/b/be/Cristiano_Ronaldo%2C_2023.jpg"),
+    artist: "Rihanna",
+  ),
+  MediaItem(
+    id: "assets://assets/Rihanna - Cheers (Drink To That).mp3",
+    title: "Cheers (Drink To That)",
+    artUri: Uri.parse("https://upload.wikimedia.org/wikipedia/commons/b/be/Cristiano_Ronaldo%2C_2023.jpg"),
+    artist: "Rihanna",
+  ),
+  MediaItem(
+    id: "assets://assets/Rihanna - Complicated with Lyrics.mp3",
+    title: "Complicated with Lyrics",
+    artUri: Uri.parse("https://upload.wikimedia.org/wikipedia/commons/b/be/Cristiano_Ronaldo%2C_2023.jpg"),
+    artist: "Rihanna",
+  ),
+  MediaItem(
+    id: "assets://assets/Rihanna - Fading (Lyrics).mp3",
+    title: "Fading (Lyrics)",
+    artUri: Uri.parse("https://upload.wikimedia.org/wikipedia/commons/b/be/Cristiano_Ronaldo%2C_2023.jpg"),
+    artist: "Rihanna",
+  ),
+  MediaItem(
+    id: "assets://assets/Rihanna - Love The Way You Lie ft. Eminem(Part 2).flv.mp3",
+    title: "Love The Way You Lie ft. Eminem (Part 2)",
+    artUri: Uri.parse("https://upload.wikimedia.org/wikipedia/commons/b/be/Cristiano_Ronaldo%2C_2023.jpg"),
+    artist: "Rihanna",
+  ),
+  MediaItem(
+    id: "assets://assets/Rihanna - Man Down.mp3",
+    title: "Man Down",
+    artUri: Uri.parse("https://upload.wikimedia.org/wikipedia/commons/b/be/Cristiano_Ronaldo%2C_2023.jpg"),
+    artist: "Rihanna",
+  ),
+  MediaItem(
+    id: "assets://assets/Rihanna - Only Girl (In The World).mp3",
+    title: "Only Girl (In The World)",
+    artUri: Uri.parse("https://upload.wikimedia.org/wikipedia/commons/b/be/Cristiano_Ronaldo%2C_2023.jpg"),
+    artist: "Rihanna",
+  ),
+  MediaItem(
+    id: "assets://assets/Rihanna - Raining men.mp3",
+    title: "Raining men",
+    artUri: Uri.parse("https://upload.wikimedia.org/wikipedia/commons/b/be/Cristiano_Ronaldo%2C_2023.jpg"),
+    artist: "Rihanna",
+  ),
+  MediaItem(
+    id: "assets://assets/Rihanna - S_u0026M.mp3",
+    title: "S_u0026M",
+    artUri: Uri.parse("https://upload.wikimedia.org/wikipedia/commons/b/be/Cristiano_Ronaldo%2C_2023.jpg"),
+    artist: "Rihanna",
+  ),
+  MediaItem(
+    id: "assets://assets/Rihanna - What's My Name? ft. Drake.mp3",
+    title: "What's My Name? ft. Drake",
+    artUri: Uri.parse("https://upload.wikimedia.org/wikipedia/commons/b/be/Cristiano_Ronaldo%2C_2023.jpg"),
+    artist: "Rihanna",
+  ),
+  MediaItem(
+    id: "assets://assets/Skin - Rihanna (Lyrics).mp3",
+    title: "Skin (Lyrics)",
+    artUri: Uri.parse("https://upload.wikimedia.org/wikipedia/commons/b/be/Cristiano_Ronaldo%2C_2023.jpg"),
+    artist: "Rihanna",
   ),
 ];
 
