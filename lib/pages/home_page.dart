43d255090be39a38b141a38eb6bf672e4c18895f
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-12 17:32:12 +0500

    imp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,13 @@
+import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
+
+List<MediaItem> mediaItems = [
+  MediaItem(
+    id: "assets://assets/Rihanna - California King Bed.mp3",
+    title: "California King Bed",
+
+  ),
+];
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
